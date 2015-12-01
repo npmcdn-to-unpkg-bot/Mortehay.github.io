02f13ba590d3091ba1cc6eb3e6072476f0fef725
Mortehay <u.shpilevoy@gmail.com>
2015-12-01 13:57:40 +0200

    обновил урок10-11_1

--- GoIT/markup/lesson10-11_1/task10-11_1.js
+++ GoIT/markup/lesson10-11_1/task10-11_1.js
@@ -1,7 +1,23 @@
 function pow() {
 	NumberToPower=prompt('enter NumberToPower',0);
 	PowerOfNumber=prompt('enter PowerOfNumber',0);
-	pow=Math.pow(NumberToPower,PowerOfNumber);
-	console.log(pow);
+	var ResultNumber=1;
+	if (PowerOfNumber>=0) {
+		for (var i = 0; i < PowerOfNumber; i++) {
+		ResultNumber=ResultNumber*NumberToPower;
+		};
+		console.log('ResultNumber=', ResultNumber);
+		pow=Math.pow(NumberToPower,PowerOfNumber);
+		console.log('pow=', pow);
+		} 
+	else{
+		for (var i = 0; i < Math.abs(PowerOfNumber); i++) {
+		ResultNumber=ResultNumber*NumberToPower;
+		};
+		ResultNumber=1/(ResultNumber);
+		console.log('ResultNumber=', ResultNumber);
+		pow=Math.pow(NumberToPower,PowerOfNumber);
+		console.log('pow=', pow);
+	};
 }
 pow();
