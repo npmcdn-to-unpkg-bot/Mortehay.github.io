0c430840ef1bdcde242d6bf00984d72cab9a6ec1
Mortehay <u.shpilevoy@gmail.com>
2015-12-01 15:35:19 +0200

    обновил урок 10-11_2

--- GoIT/markup/lesson10-11_2/task10-11_2.js
+++ GoIT/markup/lesson10-11_2/task10-11_2.js
@@ -1,19 +1,31 @@
 var Names=[];
 for (var i = 0; i < 5; i++) {
 	Names[i]=prompt('введите имя №'+(+i+1), 'имя');
+	if (Names[i] == 'имя' || Names[i] == null) {
+		alert('Пожалуйса, введите имя');
+		window.location.reload(false);  
+	}
 }
 console.log(Names);
 var CheckName=prompt('введите имя пользователя', 'имя');
 console.log('CheckName='+CheckName);
 console.log('Names.length='+Names.length);
-for (var i = 0; i < Names.length; i++) {
-	console.log('CounterI='+i);
-	if (Names[i]==CheckName) break;	
-}
-console.log('CheckCounterI='+i);
-if (i<Names.length) {
-	alert(Names[i]+', вы успешно вошли');
-} else {
-	alert('ERROR');
-}
+if (CheckName=='имя') {
+	alert('Пожалуйса, введите имя');
+	window.location.reload(false); 
+} else{
+	for (var i = 0; i < Names.length; i++) {
+		console.log('CounterI='+i);
+		if (Names[i]==CheckName) break;	
+	}
+	console.log('CheckCounterI='+i);
+	if (i<Names.length) {
+		alert(CheckName+', вы успешно вошли');
+	} else {
+		alert('ERROR');
+	}
+};
+
+
+
 
