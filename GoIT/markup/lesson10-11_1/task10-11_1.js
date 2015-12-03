c3f987e125c9951d31448d44f4ee5bd3fbef6a80
Mortehay <u.shpilevoy@gmail.com>
2015-12-03 14:29:03 +0200

    обновил урок 10-11_1

--- GoIT/markup/lesson10-11_1/task10-11_1.js
+++ GoIT/markup/lesson10-11_1/task10-11_1.js
@@ -1,23 +1,31 @@
 function pow() {
-	NumberToPower=prompt('enter NumberToPower',0);
-	PowerOfNumber=prompt('enter PowerOfNumber',0);
-	var ResultNumber=1;
-	if (PowerOfNumber>=0) {
-		for (var i = 0; i < PowerOfNumber; i++) {
-		ResultNumber=ResultNumber*NumberToPower;
-		};
-		console.log('ResultNumber=', ResultNumber);
-		pow=Math.pow(NumberToPower,PowerOfNumber);
-		console.log('pow=', pow);
-		} 
-	else{
-		for (var i = 0; i < Math.abs(PowerOfNumber); i++) {
-		ResultNumber=ResultNumber*NumberToPower;
-		};
-		ResultNumber=1/(ResultNumber);
-		console.log('ResultNumber=', ResultNumber);
-		pow=Math.pow(NumberToPower,PowerOfNumber);
-		console.log('pow=', pow);
+	var NumberToPower=+prompt('enter NumberToPower',0);
+	if ( (NumberToPower==null) || (NumberToPower=='0') ) {
+		return console.log('function aborted. no NomberToPower');
+	} else{
+		var PowerOfNumber=+prompt('enter PowerOfNumber',0);
+		if ( (PowerOfNumber==null) || (PowerOfNumber=='0') ) {
+			return console.log('function aborted. no PowerToNumber');					
+		} else{
+				var ResultNumber=1;
+				if (PowerOfNumber>=0) {
+					for (var i = 0; i < PowerOfNumber; i++) {
+					ResultNumber=ResultNumber*NumberToPower;
+					};
+					console.log('ResultNumber=', ResultNumber);
+					pow=Math.pow(NumberToPower,PowerOfNumber);
+					console.log('pow=', pow);
+					} 
+				else{
+					for (var i = 0; i < Math.abs(PowerOfNumber); i++) {
+					ResultNumber=ResultNumber*NumberToPower;
+					};
+					ResultNumber=1/(ResultNumber);
+					console.log('ResultNumber=', ResultNumber);
+				pow=Math.pow(NumberToPower,PowerOfNumber);
+				console.log('pow=', pow);
+				};		
+		};		
 	};
 }
 pow();
