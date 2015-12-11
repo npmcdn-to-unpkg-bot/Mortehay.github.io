e28f793ef2337d666d2d048aa905d276cf34356f
Yurii <u.shpilevoy@gmail.com>
2015-12-12 01:22:00 +0200

    third try

--- GoIT/markup/js_03-04/js_03-04.js
+++ GoIT/markup/js_03-04/js_03-04.js
@@ -1,69 +1,91 @@
-document.body.style.backgroundColor="grey";
-var WraperDiv=document.createElement('div');
-WraperDiv.className="WraperDiv";
-WraperDiv.style.width="600px";
-WraperDiv.style.height="600px";
-WraperDiv.style.margin="50px auto 50px auto";
-WraperDiv.style.backgroundColor="#ffffff";
-WraperDiv.style.border="1px solid #000000";
-document.body.appendChild(WraperDiv);
-var H3text = document.createElement('h3');
-H3text.className = "H3Text";
-H3text.innerHTML = "Тест по программированию";
-H3text.style.width="250px";
-H3text.style.marginTop="50px";
-H3text.style.marginLeft="auto";
-H3text.style.marginRight="auto";
-WraperDiv.appendChild(H3text);
-var question={
-	set0: ['<p>Вопрос №1</p>', '<p>Вопрос №2</p>', '<p>Вопрос №3</p>'],
-	set1: ['<p>Вопрос №1</p>', '<p>Вопрос №2</p>', '<p>Вопрос №3</p>', '<p>Вопрос №4</p>']
+
+var questionsAmount={
+	set0: ['Вопрос №1', 'Вопрос №2', 'Вопрос №3'],
+	set1: ['Вопрос №1', 'Вопрос №2', 'Вопрос №3', 'Вопрос №4']
 };
-var option={
+var answersAmount={
 	set0: ['<p>Вариант ответа №1</p>', '<p>Вариант ответа №2</p>', '<p>Вариант ответа №3</p>'],
 	set1: ['<p>Вариант ответа №1</p>', '<p>Вариант ответа №2</p>', '<p>Вариант ответа №3</p>', '<p>Вариант ответа №4</p>']
 };
-function MakeLists (array1, array2) {
-	var OuterList=document.createElement('ol');
-	OuterList.style.width="250px";
-	OuterList.style.marginTop="50px";
-	OuterList.style.marginLeft="10px";
-	/*OuterList.style.marginRight="auto";*/
-	for (var i = 0; i < array1.length; i++) {
-		var OuterItem=document.createElement('li');
-		OuterItem.innerHTML=array1[i];
-		OuterList.appendChild(OuterItem);
-		OuterItem.style.marginBottom="15px";		
-		var InnerList=document.createElement('ul');
-		InnerList.style.marginTop="15px";
-		for (var j = 0; j < array2.length; j++) {
-			var InnerItem=document.createElement('li');
-			InnerItem.innerHTML=array2[j];
-			InnerList.appendChild(InnerItem);
-			InnerList.style.listStyleType="none";
-			var CheckboxUl = document.createElement("INPUT");
-			CheckboxUl.setAttribute("type", "checkbox");
-			CheckboxUl.className="regular-checkbox";
-			InnerList.insertBefore(CheckboxUl, InnerItem);
-			/*CheckboxUl.style.margin="0";*/
-			InnerItem.style.display="block";
-			InnerItem.style.marginLeft="10px";
-			CheckboxUl.style.cssFloat="left";
-			
-			/*CheckboxUl.style.border="1px solid #444b7e";*/
-		}
-		OuterItem.appendChild(InnerList);
+var app = {
 
-	};
-	WraperDiv.appendChild(OuterList);
+	createElement: function(params) {
+	    var element = document.createElement(params.tagName);
+
+	    if (params.inputType){
+	      element.setAttribute('type', params.inputType);
+	    }
+
+	    if (params.className){
+	      element.className = params.className;
+	    }
+
+	    if (params.content){
+	      element.innerHTML = params.content;
+	    }
+
+	    if (params.parentElement){
+	      params.parentElement.appendChild(element);
+	    }
+	       
+	    return element;
+	  },
+
+	generateQuestions: function(questionsAmount, answersAmount) {
+
+	    for (var i = 0; i < questionsAmount.length; i++) {
+	      this.createElement({
+	        tagName: 'h2',
+	        content: questionsAmount[i],
+	        parentElement: form
+	      });
+
+	      for (var j = 0; j < answersAmount.length; j++) {
+	        var label = this.createElement({
+	          tagName: 'label',
+	          content: answersAmount[j],
+	          parentElement: form,
+	          styleDisplay: 'inline-block'
+	        });
+
+	        var checkbox = this.createElement({
+	          tagName: 'input',
+	          inputType: 'checkbox'
+	        });
+
+	        label.insertAdjacentElement('afterBegin', checkbox);
+	      }
+	    };
+	  }
+
+	
 }
-MakeLists(question.set0, option.set0);
-var CheckButton = document.createElement("INPUT");
-CheckButton.setAttribute("type", "button");
-CheckButton.value="Проверить мои результаты";
-CheckButton.style.display="box";
-CheckButton.style.padding="5px 30px 5px 30px";
-CheckButton.style.backgroundColor="#98dafb";
-CheckButton.style.borderColor="#444b7e";
-CheckButton.style.margin="30px auto 30px 30%";
-WraperDiv.appendChild(CheckButton);
+var body = document.querySelector('body');
+
+app.createElement({
+  tagName: 'div',
+  parentElement: body,
+  className : "wraperDiv"
+});
+var wraperDiv =document.querySelector('div')
+
+app.createElement({
+  tagName: 'h1',
+  content: 'Тест по программированию',
+  parentElement: wraperDiv
+});
+
+var form = app.createElement({
+  tagName: 'form',
+  parentElement: wraperDiv,
+});
+
+app.generateQuestions(questionsAmount.set0, answersAmount.set0);
+
+app.createElement({
+  tagName: 'input',
+  inputType: 'submit',
+  content: 'Проверить мои результаты',
+  parentElement: form
+});
+
