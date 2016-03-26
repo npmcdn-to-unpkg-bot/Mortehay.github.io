07a15fc14ecd92b97224c08ddda9d72956cbc7d1
Yurii <u.shpilevoy@gmail.com>
2016-03-26 23:37:07 +0200

    1

---
+++ test/test1/js/working/main.js
@@ -0,0 +1,9 @@
+//--jquery main---------------------------------------
+$( document ).ready(function(){
+	$('.jcarousel').jcarousel({
+		list:'.jcarousel-list',
+		items: '.jcarousel-item'
+	});
+
+});
+//----------------------------------------------------
