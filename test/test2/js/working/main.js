b31b3d6ea75419e0ff1d9d78f1072f0b1097135f
Yurii <u.shpilevoy@gmail.com>
2016-03-29 20:12:23 +0300

    1

---
+++ test/test2/js/working/main.js
@@ -0,0 +1,65 @@
+//grlobal variebles-----------------------------------
+var info=[];
+var starArr=[];
+var imgArr=['img/img_1.png',
+'img/img_2.png',
+'img/img_3.png'];
+var titleArr=['NEW ZELAND TIMELAPS', 'NEW ZELAND TIMELAPS', 'NEW ZELAND TIMELAPS'];
+
+var textArr=['	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate mollitia praesentium possimus incidunt ea optio sapiente, ratione adipisci non! Ex beatae ',
+'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis obcaecati veritatis ad repellat quas nam ratione aut deserunt, possimus magni deleniti eum, dolore qua',
+'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In reprehenderit totam sequi iusto atque fugiat culpa sint soluta, quos quod, ipsam officia natus',
+];
+
+//--jquery main---------------------------------------
+$( document ).ready(function(){
+
+	$('.template').template({id:'#template', arr:info});
+});
+//----------------------------------------------------
+
+//some code-------------------------------------------
+for (var i = 0; i < imgArr.length; i++) {
+	starNumber=Math.round( (10*Math.random())/2);
+	var stars='';
+	for (var i = 1; i < 6; i++) {
+		if (i<=starNumber) {
+			stars=stars+'<span class="blackStar">&#9733</span>';
+		} else{
+			stars=stars+'<span class="whiteStar">&#9733</span>';
+		};
+	};
+	starArr.push(stars);
+	console.log('starArr', starArr);
+};
+for (var i = 0; i<imgArr.length; i++) {
+		info.push({
+			imgLink: imgArr[i],
+			stars: starArr[i],
+			imgTitle: titleArr[i],
+			imgText: textArr[i]
+		});
+};
+
+
+//----------------------------------------------------
+
+//jquery plugins--------------------------------------
+(function($){				
+
+//template plugin-------------------------------------
+	$.fn.template = function(options){
+		var html=$(options.id).html();
+		
+	
+		console.log('arr', options.arr);
+
+		var content = tmpl(html, {
+			data: options.arr
+		});
+		$(this).append(content);
+
+	};
+//-----------------------------------------------------	
+	
+})(jQuery);
