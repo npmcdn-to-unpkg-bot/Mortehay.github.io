ee12a3f4dae0e68bbb8a8eb50936d6708df36008
Yurii <u.shpilevoy@gmail.com>
2016-04-05 23:33:48 +0300

    not completed v3

--- test/test3/js/working/main.js
+++ test/test3/js/working/main.js
@@ -1,29 +1,34 @@
 //grlobal variebles-----------------------------------
 var links={
-	link1:'https://maps.googleapis.com/maps/api/staticmap?center=0,0&zoom=1&size=640x400&path=weight:3%7Ccolor:blue%7Cenc:aofcFz_bhVJ[n@ZpAp@t@b@uA`FuAzEoCdJiDpLs@VM@y@s@oBcBkAw@cCoAuBu@eEaAiAa@iAi@w@a@o@g@g@k@e@u@uAaCc@i@w@y@eAo@i@UaBc@kAGo@@]JyKA}EC{G?q@?IGKCeGA{CAyCAyEAwEBaFAkJ?yGEyAIiLAiB?{@BcBJ}@@aBGwBEo@A@j@BjBFTHjEl@fOD`C?|@RARAJERWPL@FE^S`AI`A&key=AIzaSyBr408DpWKxQskSBZyFLzdHVoKTcPYQxqU',
-	link2:'https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=0,0&zoom=1&size=640x400&key=AIzaSyBr408DpWKxQskSBZyFLzdHVoKTcPYQxqU'
+	link1:'https://maps.googleapis.com/maps/api/staticmap?center=0,0&zoom=1&size=640x640&path=weight:3%7Ccolor:blue%7Cenc:aofcFz_bhVJ[n@ZpAp@t@b@uA`FuAzEoCdJiDpLs@VM@y@s@oBcBkAw@cCoAuBu@eEaAiAa@iAi@w@a@o@g@g@k@e@u@uAaCc@i@w@y@eAo@i@UaBc@kAGo@@]JyKA}EC{G?q@?IGKCeGA{CAyCAyEAwEBaFAkJ?yGEyAIiLAiB?{@BcBJ}@@aBGwBEo@A@j@BjBFTHjEl@fOD`C?|@RARAJERWPL@FE^S`AI`A&key=AIzaSyBr408DpWKxQskSBZyFLzdHVoKTcPYQxqU',
+	link2:'https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=0,0&zoom=1&size=640x640&key=AIzaSyBr408DpWKxQskSBZyFLzdHVoKTcPYQxqU'
 }
 var paramsCanvas=[
 	{	name : "London",
-		lon : 51.5085300,
-		lat : -0.1257400
+		lat : 51.5085300,
+		lon : -0.1257400
 	},
 	{	name : "New York",
-		lon : 40.7142700,
-		lat : -74.0059700
+		lat : 40.7142700,
+		lon : -74.0059700
 	},
 	{	name: "Moskow",
-		lon: 55.7522200,
-		lat: 37.6155600
+		lat: 55.7522200,
+		lon: 37.6155600
 	},
 	{	name: "Sydney",
-		lon: -33.8678500,
-		lat: +151.2073200
+		lat: -33.8678500,
+		lon: +151.2073200
 	},
 	{	name: "Buenos Aires",
-		lon: -34.6131500,
-		lat: -58.3772300
+		lat: -34.6131500,
+		lon: -58.3772300
 	},
+	{
+		name: "Zero",
+		lon:0,
+		lat:0
+	}
 ];
 //some-code-------------------------------------------
 
@@ -40,21 +45,12 @@ function mainloadCanvas() {
 		var background = new Image();
 		background.src = links.link1;
 	
-		/*background.onload = function(){*/
-		    	ctx.drawImage(background,0,0);
-		 /*};*/
-		var can2 = document.getElementById('canvas');
-		var ctx2 = can2.getContext('2d');
 
-		ctx2.beginPath();
-		  ctx2.fillStyle = "pink";
-		  ctx2.arc(50, 50, 50, 0, Math.PI * 2, 1);
-		  ctx2.fill();
-		  ctx2.beginPath();
-		  ctx2.clearRect(20, 40, 60, 20);
-		  
-		    	ctx.drawImage(can2,0,0);
-		 
+		 background.onload = function(){	
+			ctx.drawImage(background,0,0);
+		 };
+
+ 
 };
 //---------------------------------------------------------------
 //reload-canvas-------------------------------------------
@@ -76,25 +72,30 @@ function reloadCanvas() {
 		if ( $('#progectionType').val()  =='option2') {
 			background.src = links.link2;
 		};
-		
-		/*background.onload = function(){*/
-		    ctx.drawImage(background,0,0);
-		 /*};*/	
-		var can2 = document.getElementById('canvas');
-		var ctx2 = can2.getContext('2d');
 
-		ctx2.beginPath();
-		  ctx2.fillStyle = "pink";
-		  ctx2.arc(50, 50, 50, 0, Math.PI * 2, 1);
-		  ctx2.fill();
-		  ctx2.beginPath();
-		  ctx2.clearRect(20, 40, 60, 20);
-		  
-		    	ctx.drawImage(can2,0,0);
+		 background.onload = function(){
+		    	ctx.drawImage(background,0,0);
+
+
+		 };
+		    	
 	});
 };
 function pointCanvas() {
+	var pointCanvas = document.getElementById('points');
+	var point = pointCanvas.getContext('2d');
 
+	pointCanvas.width = 640;
+	pointCanvas.height = 640;
+	point.fillStyle = "#ff0000";
+	for (var i = 0; i < paramsCanvas.length; i++) {
+		point.fillRect((320+(320*paramsCanvas[i].lon)/228 ),(320-(320*paramsCanvas[i].lat)/191 ),5,5);
+		console.log('lon', 320+(320*paramsCanvas[i].lon)/90);
+		console.log('lat', 320+(320*paramsCanvas[i].lat)/90);
+	}
+	var img= new Image();
+	img.src=pointCanvas.toDataURL("image/png");
+	point.drawImage(img,0,0);
 
 };
 //-----------------------------------------------------------
@@ -102,9 +103,11 @@ function pointCanvas() {
 //--jquery main---------------------------------------
 $( document ).ready(function(){
 	
+	
+	
 	mainloadCanvas();
 	reloadCanvas();
-	
+	pointCanvas();
 });	
 	
 	
