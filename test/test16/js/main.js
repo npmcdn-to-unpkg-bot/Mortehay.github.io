3736e556cf4b4feabcba87637590acb5899b899d
Yurii <u.shpilevoy@gmail.com>
2016-08-27 21:41:21 +0300

    third try

--- test/test16/js/main.js
+++ test/test16/js/main.js
@@ -1,23 +1,8 @@
-var string = 'Вт: 15:00, Пн: 8:00, Чт: 12:00, Вт: 15:30, Вт: 16:00, Вт: 17:00, Чт: 11:00, Ср: 14:30, Ср: 17:30, Вт: 10:00, Чт: 11:30';
-var input;
-var output;
-var arrayInput = [];
-var arrayTemp = [];
-var arrayIndex = [];
-var arrayIndexNew = [];
-var arrayIndexNew2D = [];
-var arrayOutput = [];
-var temp= -10000;
-var dayx;
-var time;
-//var tempL='';
-var tempTime;
-var value;
-var theOrder = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс' ];
-var arraySorted;
+var string = 'Вт: 15:00, Пн: 8:00, Чт: 12:00, Вт: 15:30, Вт: 16:00, Вт: 17:00, Чт: 11:00, Ср: 14:30, Вт: 14:00, Ср: 17:30, Вт: 10:00, Чт: 11:30';
 
-function sortFunction(a, b) {
-    return (a[1] < b[1] ? -1 : (a[1] > b[1] ? 1 : 0)); }
+
+
+
 
 //----------------------------------------------
 
@@ -37,8 +22,21 @@ $(document).ready(function(){
 $.fn.clickFunction = function(){
 	
 	this.on('click', function(){
-
-
+		var input;
+		var output;
+		var value;
+		var arraySorted =[];
+		var arrayInput = [];
+		var arrayTemp = [];
+		var arrayIndex = [];
+		var arrayIndexNew = [];
+		var arrayIndexNew2D = [];
+		var arrayOutput = [];
+		var temp= -10000;
+		var day = '';
+		var time = '';
+		var tempTime = 0;
+		var theOrder = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс' ];
 		//input = $('#input').val();
 		input = string;
 		if (input =='' ){
@@ -75,22 +73,25 @@ $.fn.clickFunction = function(){
 			console.log('arraySorted', arraySorted); //sorted array
 			//---------------------------
 			for (var j = 0; j < theOrder.length; j++) {
-				
+				temp = -100000;
 				for( var z = 0; z < arraySorted.length; z++) {
 				
-					if (arraySorted[z][0] == j){//console.log(arraySorted[z][1]);
-						if( Math.abs(temp - arraySorted[z][1]) <= 0.5) {
+					if (arraySorted[z][0] == j){//console.log(arraySorted[z][1])
+						arrayIndexNew2D.push(['','','']);	
+						if( Math.abs(temp  - arraySorted[z][1]) <= 0.5) {
 							console.log(temp - arraySorted[z][1]);
 							console.log(z);
 							arrayIndexNew.push(z);
-							arrayIndexNew2D.push([arraySorted[z][0],z]);						
-						};		
+							arrayIndexNew2D.push([arraySorted[z][0],arraySorted[z][1],z]);						
+						};
+
 					}
 					temp = arraySorted[z][1];
+
 				}
 				
 				console.log('---------------------------');
-				temp = -100000;
+				
 			}
 			console.log('arrayIndexNew', arrayIndexNew);
 			console.log('arrayIndexNew2D', arrayIndexNew2D);
@@ -100,16 +101,32 @@ $.fn.clickFunction = function(){
 				day = theOrder[s];
 				for (var w = 0; w < arraySorted.length; w++) {
 					if (arraySorted[w][0] == s) {
-						if ( (arraySorted[w][1] - Math.floor(arraySorted[w][1]) >0) ) {
+						if (arrayIndexNew.indexOf(w) >= 0) {
+							console.log('---------');
+							if ( (arraySorted[w][1] - Math.floor(arraySorted[w][1]) >0) ) {
 							time = Math.floor(arraySorted[w][1]) + ':' + ((arraySorted[w][1] - Math.floor(arraySorted[w][1]) ) +'').replace('0.5','30');
+							} else {
+								time = arraySorted[w][1] + ':00';
+							}
+
+							if (tempTime =='') {
+								tempTime = tempTime+time;
+							} else {
+								tempTime = tempTime+' - '+time;
+							}
 						} else {
-							time = arraySorted[w][1] + ':00';
+							if ( (arraySorted[w][1] - Math.floor(arraySorted[w][1]) >0) ) {
+							time = Math.floor(arraySorted[w][1]) + ':' + ((arraySorted[w][1] - Math.floor(arraySorted[w][1]) ) +'').replace('0.5','30');
+							} else {
+								time = arraySorted[w][1] + ':00';
+							}
+							if (tempTime =='') {
+								tempTime = tempTime+time;
+							} else {
+								tempTime = tempTime+', '+time;
+							}
 						}
-						if (tempTime=='') {
-							tempTime=tempTime+time;
-						} else {
-							tempTime=tempTime+', '+time;
-						}
+						
 						
 					}
 					
@@ -129,10 +146,11 @@ $.fn.clickFunction = function(){
 			value = arrayTemp.toString();
 			//value = arrayOutput.toString();
 			console.log('arrayOutput', arrayOutput);
+			$('.result').find('ul').remove('ul');
 			$('#output').val(value);
 			var list = $('.result').append('<ul></ul>').find('ul');
-			for (var i = 0; i < arrayTemp.length; i++){
-			    list.append('<li>'+arrayTemp[i]+'</li>');
+			for (var count = 0; count < arrayTemp.length; count++){
+			    list.append('<li>'+arrayTemp[count]+'</li>');
 			}
 			
 
